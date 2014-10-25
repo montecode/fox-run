277f5cd5f64cd2f362099072acaaa61a09d850df
Aleksa Doderović <aleksadoderovic@gmail.com>
2014-10-25 22:08:00 +0200

    Dodge bug fixed

--- core/src/me/montecode/games/runningmonster/utils/Constants.java
+++ core/src/me/montecode/games/runningmonster/utils/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
     public static final float RUNNER_GRAVITY_SCALE = 3f;
     public static float RUNNER_DENSITY = 0.5f;
     public static final float RUNNER_DODGE_X = 2f;
-    public static final float RUNNER_DODGE_Y = 1.5f;
+    public static final float RUNNER_DODGE_Y = RUNNER_Y - 0.5f;
     public static final Vector2 RUNNER_JUMPING_LINEAR_IMPULSE = new Vector2(0, 30f);
     public static final float RUNNER_HIT_ANGULAR_IMPULSE = 40f;
 
